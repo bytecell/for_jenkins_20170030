c40966afb9758ba60c4734271d0b92fc887b9555
Young-Seob Jeong <bytecell@sch.ac.kr>
2021-02-12 02:16:01 +0900

    for studying Sphinx

--- adam.py
+++ adam.py
@@ -1,1 +1,20 @@
-print('hello')
+"""Just for practicing Sphinx"""
+
+def sum_two(a, b):
+    """Compute sum of two values, and return int"""
+    return a + b
+
+def get_biggest(x):
+    """Return the biggest value
+
+    Key arguments:
+    x -- list of int
+    """
+    ret = x[0]
+    for i in range(1, len(x)):
+        if x[i] > ret:
+            ret = x[i]
+    return ret
+
+print('sum_two() result = ', sum_two(1, 3))
+print('get_biggest() result = ', get_biggest([5,3,4,-10,0]))
