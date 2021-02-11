37c66d9eaa230c1149feb9beb7d5aa63ec817ae3
Young-Seob Jeong <bytecell@sch.ac.kr>
2021-02-12 03:42:28 +0900

    del all things about sphinx

--- adam.py
+++ adam.py
@@ -1,20 +1,22 @@
 """Just for practicing Sphinx"""
 
-def sum_two(a, b):
-    """Compute sum of two values, and return int"""
-    return a + b
+class adam:
+    def sum_two(self, a, b):
+        """Compute sum of two values, and return int"""
+        return a + b
+    
+    def get_biggest(self, x):
+        """Return the biggest value
+    
+        Key arguments:
+        x -- list of int
+        """
+        ret = x[0]
+        for i in range(1, len(x)):
+            if x[i] > ret:
+                ret = x[i]
+        return ret
 
-def get_biggest(x):
-    """Return the biggest value
-
-    Key arguments:
-    x -- list of int
-    """
-    ret = x[0]
-    for i in range(1, len(x)):
-        if x[i] > ret:
-            ret = x[i]
-    return ret
-
-print('sum_two() result = ', sum_two(1, 3))
-print('get_biggest() result = ', get_biggest([5,3,4,-10,0]))
+a = adam()
+print('sum_two() result = ', a.sum_two(1, 3))
+print('get_biggest() result = ', a.get_biggest([5,3,4,-10,0]))
